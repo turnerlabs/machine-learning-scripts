d66bc0643b83244c0eab5c77d4283df144a629d7
Josh Kurz <josh.kurz@turner.com>
2017-02-09 13:26:06 -0500

    feat(book-reviews): tweak script to weight text
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- book-reviews/csv_converter.py
+++ book-reviews/csv_converter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import csv
 import sys
+import operator
 
 # arguments
 # @args[1] csvFile: the csvFile to convert
@@ -24,8 +25,31 @@ except:
     polar = False
 
 
+def get_weighted_text(text_array):
+    word_map = {}
+    allowed_length = 5
+    for word in text_array:
+        if len(word) > 3:
+            if not word_map.get(word):
+                word_map[word] = 0
+
+            word_map[word] += 1
+
+    # returns an array tuples [("word", count), ("new_word", count)]
+    sorted_words = sorted(word_map.items(), key=operator.itemgetter(1), reverse=True)
+    # get rid of columns less than n and add padding for columns less than n
+    if len(sorted_words) <= allowed_length:
+        while len(sorted_words) <= allowed_length:
+            sorted_words.append(("", 0))
+
+    sorted_words = sorted_words[0:allowed_length]
+
+    return sorted_words
+
+
+
 with open("{0}_result.csv".format(csvFile), 'w') as csvfile_write:
-    fieldnames = ['score', 'text']
+    fieldnames = ['score', 'word_0', 'value_0', 'word_1', 'value_1', 'word_2', 'value_2', 'word_3', 'value_3', 'word_4', 'value_4']
     writer = csv.DictWriter(csvfile_write, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -34,7 +58,19 @@ with open("{0}_result.csv".format(csvFile), 'w') as csvfile_write:
         for row in reader:
             review = row[0]
             text = row[3].replace('<span class="a-size-base review-text">', '').replace('</span>', '')
+            text = "{0} {1}".format(row[2], text)
+            text_array = text.split(" ")
+            # this is where we are going to extract the characters from the array blob and assign weighted values
+            weighted_text = get_weighted_text(text_array)
             if row[0] == "5.0":
-                writer.writerow({'score': 1, 'text': text})
+                row_object = {'score': 1}
             elif row[0] == "1.0" or polar == True:
-                writer.writerow({'score': 0, 'text': text})
+                row_object = {'score': 0}
+
+            for index, values in enumerate(weighted_text):
+                word_key = "word_{0}".format(index)
+                value_key = "value_{0}".format(index)
+                row_object[word_key] = values[0]
+                row_object[value_key] = values[1]
+
+            writer.writerow(row_object)
