9843e159ffd34e93ce885f8247afc55cf433900a
Josh Kurz <josh.kurz@turner.com>
2017-02-16 12:13:20 -0500

    changing around converter
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- book-reviews/csv_converter.py
+++ book-reviews/csv_converter.py
@@ -2,6 +2,7 @@
 import csv
 import sys
 import operator
+from BeautifulSoup import BeautifulSoup
 
 # arguments
 # @args[1] csvFile: the csvFile to convert
@@ -26,51 +27,68 @@ except:
 
 
 def get_weighted_text(text_array):
-    word_map = {}
-    allowed_length = 5
+    new_words = []
+    #allowed_length = 40
     for word in text_array:
-        if len(word) > 3:
-            if not word_map.get(word):
-                word_map[word] = 0
-
-            word_map[word] += 1
+        # if len(word) > 3:
+        new_words.append(word)
 
     # returns an array tuples [("word", count), ("new_word", count)]
-    sorted_words = sorted(word_map.items(), key=operator.itemgetter(1), reverse=True)
+    # sorted_words = sorted(word_map.items(), key=operator.itemgetter(1), reverse=True)
+    # for word in new_words:
+    #     count = 0
+    #     while count < word[1]:
+    #         new_words.append(word[0])
+    #         count += 1
+
     # get rid of columns less than n and add padding for columns less than n
-    if len(sorted_words) <= allowed_length:
-        while len(sorted_words) <= allowed_length:
-            sorted_words.append(("", 0))
+    # if len(new_words) <= allowed_length:
+    #     while len(new_words) <= allowed_length:
+    #         new_words.append("")
 
-    sorted_words = sorted_words[0:allowed_length]
+    #new_words = new_words[0:allowed_length]
 
-    return sorted_words
+    return ' '.join(new_words)
 
 
 
 with open("{0}_result.csv".format(csvFile), 'w') as csvfile_write:
-    fieldnames = ['score', 'word_0', 'value_0', 'word_1', 'value_1', 'word_2', 'value_2', 'word_3', 'value_3', 'word_4', 'value_4']
+    fieldnames = ['score', "title", "text"]
     writer = csv.DictWriter(csvfile_write, fieldnames=fieldnames)
     writer.writeheader()
+    #wrote_head = False
 
     with open(csvFile, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter='\t')
         for row in reader:
             review = row[0]
-            text = row[3].replace('<span class="a-size-base review-text">', '').replace('</span>', '')
-            text = "{0} {1}".format(row[2], text)
-            text_array = text.split(" ")
+            title = row[2]
+            text = row[3]
+            soup_text = BeautifulSoup(text)
+            soup_title = BeautifulSoup(title)
+            title = ''.join(soup_title.findAll(text=True)).encode('utf-8').strip()
+            text = ''.join(soup_text.findAll(text=True)).encode('utf-8').strip()
+            #text_array = text.split(" ")
             # this is where we are going to extract the characters from the array blob and assign weighted values
-            weighted_text = get_weighted_text(text_array)
+            row_object = {}
+            #weighted_text = get_weighted_text(text_array)
             if row[0] == "5.0":
                 row_object = {'score': 1}
             elif row[0] == "1.0" or polar == True:
                 row_object = {'score': 0}
 
-            for index, values in enumerate(weighted_text):
-                word_key = "word_{0}".format(index)
-                value_key = "value_{0}".format(index)
-                row_object[word_key] = values[0]
-                row_object[value_key] = values[1]
+            # for index, value in enumerate(weighted_text):
+            #     word_key = "word_{0}".format(index)
+            #     value_key = "value_{0}".format(index)
+                # if wrote_head == False:
+                #     fieldnames.append('word_{0}'.format(index))
+                    #fieldnames.append('value_{0}'.format(index))
+            row_object["title"] = title
+            row_object["text"] = text
+                #row_object[value_key] = values[1]
+
+            # if wrote_head == False:
+            #     writer.writeheader()
+            #     wrote_head = True
 
             writer.writerow(row_object)
